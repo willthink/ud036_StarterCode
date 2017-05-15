09ac40cdfd8ac81e776e9d8e09f006efbd2909ff
willthink <weizong2048@gmail.com>
2017-05-14 19:44:32 -0700

    change website title; Adjust comments to pass flake8

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,30 +3,30 @@ import fresh_tomatoes
 
 # create movies using media.Movie class
 avengers3 = media.Movie("Avengers 3",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BOGQ5NTY3NjktYjIzNS00Y2ZjLWIyODQtMjQzYzg1ZTMzOGI5XkEyXkFqcGdeQXVyNDMwMzEyNzA@._V1_.jpg",
+                        "https://images-na.ssl-images-amazon.com/images/M/MV5BOGQ5NTY3NjktYjIzNS00Y2ZjLWIyODQtMjQzYzg1ZTMzOGI5XkEyXkFqcGdeQXVyNDMwMzEyNzA@._V1_.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=pfQeLSDX4DQ")
 
 forrest_gump = media.Movie("Forrest Gump",
-                           "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,757,1000_AL_.jpg",
+                           "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,757,1000_AL_.jpg",  # NOQA
                            "https://www.youtube.com/watch?v=bLvqoHBptjg")
 
 shaw_red = media.Movie("Shawshank Redemption",
-                       "https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SY1000_CR0,0,672,1000_AL_.jpg",
+                       "https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SY1000_CR0,0,672,1000_AL_.jpg",  # NOQA
                        "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
 dark_knight = media.Movie("The Dark Knight",
-                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=EXeTwQWrcwY")
 
 start_wars = media.Movie("Star Wars: Episode V - The Empire Strikes Back",
-                         "https://images-na.ssl-images-amazon.com/images/M/MV5BYmViY2M2MTYtY2MzOS00YjQ1LWIzYmEtOTBiNjhlMGM0NjZjXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,644,1000_AL_.jpg",
+                         "https://images-na.ssl-images-amazon.com/images/M/MV5BYmViY2M2MTYtY2MzOS00YjQ1LWIzYmEtOTBiNjhlMGM0NjZjXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,644,1000_AL_.jpg",  # NOQA
                          "https://www.youtube.com/watch?v=JNwNXF9Y6kY")
 
 back_future = media.Movie("Back to the Future",
-                          "https://images-na.ssl-images-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,643,1000_AL_.jpg",
+                          "https://images-na.ssl-images-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,643,1000_AL_.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=qvsgGtivCgs")
 
-# add movie instances into array of movies 
+# add movie instances into array of movies
 movies = [forrest_gump, shaw_red, avengers3,
           dark_knight, start_wars, back_future]
 
--- media.py
+++ media.py
@@ -4,7 +4,6 @@ import webbrowser
 class Movie():
     """ This is the class to store movie data """
 
-    # 
     def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
         self.title = movie_title
         self.poster_image_url = poster_image_url
