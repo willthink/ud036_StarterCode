064e7c87afe6f1a9094698f14a8f1d3cf944e1b5
willthink <weizong2048@gmail.com>
2017-05-15 09:04:01 -0700

    Resubmit project
    Add docstring for init function
    Modify the import to from import
    Remove unnecessary function

--- media.py
+++ media.py
@@ -5,9 +5,17 @@ class Movie():
     """ This is the class to store movie data """
 
     def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+    	"""
+    	initialize a Movie instance 
+
+    	Args: 
+    	     movie_title: title string of the movie
+    	     poster_image_url: a url string to the poster image of the movie
+    	     trailer_youtube_url: a url string to the youtube trailer of the movie 
+
+    	Returns: 
+    	        An instance of the Movie class
+    	"""
         self.title = movie_title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-
-    def open_trailer(self):
-        webbrowser.open(self.trailer_youtube)
